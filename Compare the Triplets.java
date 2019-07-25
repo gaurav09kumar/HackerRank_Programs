62f05202a8a3a8e295e70ce0a80b61b8d2d925d4
Gaurav Kumar Rai <34624495+gaurav09kumar@users.noreply.github.com>
2019-07-25 16:10:15 +0530

    Create Compare the Triplets.java

---
+++ Compare the Triplets.java
@@ -0,0 +1,29 @@
+package programs;
+
+import java.io.*;
+import java.util.*;
+
+public class Compare the Triplets {
+	static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+		List<Integer> l1 = new ArrayList<Integer>();
+		int alice = 0, bob = 0;
+		for (int i = 0; i < a.size(); i++) {
+			if (a.get(i) == b.get(i)) {
+			} else if (a.get(i) > b.get(i)) {
+				alice++;
+			} else {
+				bob++;
+			}
+		}
+		l1.add(alice);
+		l1.add(bob);
+		return l1;
+	}
+
+	public static void main(String[] args) throws IOException {
+		List<Integer> a = Arrays.asList(5, 6, 7);
+		List<Integer> b = Arrays.asList(3, 6, 10);
+		List<Integer> result = compareTriplets(a, b);
+		System.out.println("Alice points :" + result.get(0) + "\n" + "bob points :" + result.get(1));
+	}
+}
